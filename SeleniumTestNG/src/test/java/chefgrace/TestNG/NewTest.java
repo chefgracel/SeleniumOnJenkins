7582d09b0fe9071ef310499c2db45a1e3633c111
chefgracel <graceli5341@gmail.com>
2018-07-19 13:44:28 +1200

    Create a TestNG test sample and ran successfully

--- SeleniumTestNG/src/test/java/chefgrace/TestNG/NewTest.java
+++ SeleniumTestNG/src/test/java/chefgrace/TestNG/NewTest.java
@@ -1,9 +1,31 @@
 package chefgrace.TestNG;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class NewTest {
   @Test
   public void f() {
+	  
+	  System.setProperty("webdriver.chrome.driver", "res/chromedriver"); 
+
+      WebDriver driver = new ChromeDriver();
+
+      driver.get("http://www.baidu.com/");
+
+      WebElement txtbox = driver.findElement(By.name("wd"));
+
+      txtbox.sendKeys("HelloWorld");
+
+      WebElement btn = driver.findElement(By.id("su"));
+
+      btn.click();
+      
+      driver.close();
+
   }
 }
